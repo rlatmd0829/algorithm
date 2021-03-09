0e704cc2f2f16e79c9daafff766662210f3b4f74
seungwook <rlatmd0829@naver.com>
2021-03-09 22:37:43 +0900

    알고리즘 공부

--- 백준문제/03.09/ex11651_2.py
+++ 백준문제/03.09/ex11651_2.py
@@ -4,21 +4,21 @@
 # for i in range(T):
 #     [x, y] = map(int, input().split())
 #     re = [y, x]
-#     a.append(re)
+#     a.append(re) # [[y,x], [y,x]]
 
 # a.sort()
 # for i in range(T):
 #     print(a[i][1], a[i][0])
 
-#####################
+###################################################
 
 
-def abc(x):
-    return (x[1], x[0])
+# def abc(x):
+#     return (x[1], x[0])
 
 T = int(input())
-
 a = []
+
 for i in range(T):
     a.append(list(map(int, input().split()))) # [[x,y], [x,y], [x,y]]
 a.sort(key=abc) # a 인자값을 abc로 보내서 리턴값으로 비교후 정렬
@@ -27,3 +27,5 @@ for i in a:
 
 # lambda x : (x[1], x[0]) == def abc(x):
 #                               return (x[1], x[0]) 
+
+
