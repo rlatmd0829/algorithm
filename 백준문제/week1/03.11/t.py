9e34f6ec15886ffe391bdf015ed888bd30a84cc8
seungwook <rlatmd0829@naver.com>
2021-03-18 23:34:07 +0900

    알고리즘 공부

--- 백준문제/week1/03.11/t.py
+++ 백준문제/week1/03.11/t.py
@@ -1,2 +1,13 @@
-a = '634'
-print(a[0])
+
+N, K = map(int, input().split())
+node= list(map(int, input().split()))
+node.sort()
+Sum = len(node)
+cnt = 0
+while True:
+    if Sum <= 0:
+        break
+    Sum = Sum - K + 1
+    cnt += 1
+
+print(cnt)
