2e3a1172e9e4944369243e324f7551e736cd0aaa
seungwook <rlatmd0829@naver.com>
2021-03-19 13:43:36 +0900

    알고리즘 공부

---
+++ 백준문제/week2/03.19/02_ex15649.py
@@ -0,0 +1,31 @@
+# from itertools import permutations
+# N, M = map(int, input().split())
+# node = [i for i in range(1,N+1)]
+# print(node)
+# per=permutations(node, M)
+
+# for i in per:
+#     print(*i)
+
+
+
+####################################
+
+N, M = map(int, input().split())
+visited = [False] * N # 탐사 여부 check
+out = [] # 출력 내용
+
+def solve(depth, N, M):
+    if depth == M: # 탈출 조건 이거 때문에 백트래킹이라 하는건가?
+        print(' '.join(map(str,out))) # list를 str으로 합쳐 출력
+        return
+    for i in range(len(visited)): # 탐사 check 하면서
+        if not visited[i]: # 탐사 안했다면
+            visited[i] = True # 탐사 시작(중복 제거)
+            out.append(i+1) # 탐사 내용
+            solve(depth+1, N, M) # 깊이 우선 탐색
+            visited[i] = False # 깊이 탐사 완료
+            out.pop() # 탐사 내용 제거
+
+solve(0, N, M)
+
