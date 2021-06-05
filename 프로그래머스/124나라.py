2d540364b63610d6cdc621bfd282ee2850284391
seungwook <rlatmd0829@naver.com>
2021-06-05 14:09:26 +0900

    주석 추가

--- 프로그래머스/124나라.py
+++ 프로그래머스/124나라.py
@@ -1,13 +1,16 @@
-def solution(n):
-    answer = ''
-    while n>0:
-        n -= 1
-        answer = '124'[n%3] + answer
-        n = n // 3
-    return answer
+# def solution(n):
+#     answer = ''
+#     while n>0:
+#         n -= 1
+#         answer = '124'[n%3] + answer
+#         n = n // 3
+#     return answer
 
 # 1 2 4 11 12 14
 # 1 2 3  4  5  6
 
 # 1,2,3 인 경우 1,2,4로 되고
 # 두자리인 경우도 결국 앞에 1,2,4 가 붙기 때문에 
+
+stack = [2,3,4,5]
+print(stack[-1])
--- 프로그래머스/가장큰수.py
+++ 프로그래머스/가장큰수.py
@@ -18,11 +18,14 @@ print(max(list_permute))
 
 #####################################
 
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    return str(int(''.join(numbers)))
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key=lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers)))
 
 ### ['6','10','2'] => ['666','101010','222']
 ### 문자열 정렬은 인덱스 맨 앞에값에 아스키값을 비교해 정렬을 해준다고 한다 (같을경우 다음 인덱스도 비교)
 ### reverse=True 이므로 ['6','2','10']으로 정렬 된다.
+
+stack = [2,3,4,5]
+print(stack[-1])
