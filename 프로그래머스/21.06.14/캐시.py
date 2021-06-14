2a2e3fc8d63bc95511b90adcfb43fcc745a1c293
seungwook <rlatmd0829@naver.com>
2021-06-14 20:16:54 +0900

    알고리즘 공부

---
+++ 프로그래머스/21.06.14/캐시.py
@@ -0,0 +1,17 @@
+def solution(cacheSize, cities):
+    answer = 0
+    stack = []
+    for i in cities:
+        if cacheSize == 0:
+            return 5 * len(cities)
+        i = i.lower()
+        if i in stack:
+            stack.remove(i)
+            stack.append(i)
+            answer += 1
+        else:
+            answer += 5
+            stack.append(i)
+            if len(stack) > cacheSize:
+                stack.pop(0)
+    return answer
