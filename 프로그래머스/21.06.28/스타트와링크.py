ee335e7e21f48f731be32d2f2d224f0308ea5170
seungwook <rlatmd0829@naver.com>
2021-06-30 19:49:24 +0900

    알고리즘 공부

--- 프로그래머스/21.06.28/스타트와링크.py
+++ 프로그래머스/21.06.28/스타트와링크.py
@@ -3,4 +3,12 @@ n = int(input())
 demo = [i+1 for i in range(n)]
 
 a = [list(map(int,input().split())) for _ in range(n)]
-print(list(combinations(demo,len(demo)//2)))
+arr = list(combinations(demo,len(demo)//2))
+
+# 1 3 6, 2 4 5
+# 3+2 = 5, 7, 9
+# 7, 10, 8
+
+for i in arr:
+    for j in range(len(i)-1):
+        answer = answer + a[i[j]][i[j+1]] + a[i[j+1]][i[j]]
--- 프로그래머스/21.06.30/1로만들기.py
+++ 프로그래머스/21.06.30/1로만들기.py
@@ -1,11 +1,14 @@
+# f(n) = 1 + min(f(n/3), f(n/2), f(n-1))
+# 10은 3으로 나누거나 2로 나누거나 -1뺴주거나 해준 수가 이미 있다면 그 값에다가 +1만 해주면 f(n)에 횟수가 된다.
+
+import math
 n = int(input())
-count = 0
-while n > 1:
-    count += 1
-    if n % 3 == 0:
-        n = n // 3
-    elif n % 2 == 0:
-        n = n // 2
-    else:
-        n = n-1
-print(count)
+arr = [0,0,1,1]
+for i in range(4, n+1):
+    one, two, three = math.inf, math.inf, arr[i-1]
+    if i % 3 == 0:
+        one = arr[i//3]
+    if i % 2 == 0:
+        two = arr[i//2]
+    arr.append(1 + min(one, two, three))
+print(arr[n])
