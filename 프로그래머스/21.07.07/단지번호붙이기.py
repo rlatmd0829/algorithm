b62eccb484eeb3a9b38b3ea0702761c5cec0c7b8
seungwook <rlatmd0829@naver.com>
2021-07-08 00:16:09 +0900

    알고리즘 공부

--- 프로그래머스/21.07.07/단지번호붙이기.py
+++ 프로그래머스/21.07.07/단지번호붙이기.py
@@ -1,34 +1,36 @@
+import collections
+
 n = int(input())
-
-graph = [list(map(int,input())) for _ in range(n)]
-
+graph = [list(map(int,input().strip())) for _ in range(n)]
 check = [[False]*n for _ in range(n)]
 
 def bfs(x,y):
-    danji = 0
-    queue = []
+    count = 1
+    check[x][y] = True
+    queue = collections.deque()
     queue.append((x,y))
     dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
     while queue:
-        x, y = queue.pop(0)
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n:
                 if graph[nx][ny] == 1 and check[nx][ny] == False:
-                    danji += 1
+                    count += 1
                     check[nx][ny] = True
                     queue.append((nx,ny))
-    return danji
+    return count
 
-count = 0
+
 result = []
 for x in range(n):
     for y in range(n):
         if graph[x][y] == 1 and check[x][y] == False:
             result.append(bfs(x,y))
-            count += 1
-print(count)
+            
+print(len(result))
 result.sort()
 for i in result:
     print(i)
