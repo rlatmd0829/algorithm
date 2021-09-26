116bebf16afa6e20d9e968aa312b99b8eea447a5
seungwook <rlatmd0829@naver.com>
2021-09-26 18:39:52 +0900

    알고리즘 공부

---
+++ 알고리즘풀이시즌2/21.09.26/체스판다시칠하기.py
@@ -0,0 +1,8 @@
+# n, m = map(int, input().split())
+
+# graph = [list(input()) for _ in range(n)]
+
+# for i in range(n):
+#     for j in range(m):
+#         if i == 0 and j == 0:
+#             if graph[i][j] == 'W':
